5187b00af8fbb6f68eb33e8e6620645867ee3a6c
Uriel <uespinosa770@hotmail.com>
2023-06-25 16:39:53 -0600

    se corrigio la tabla

--- Formulario/datos_personales.sql
+++ Formulario/datos_personales.sql
@@ -1,31 +1,15 @@
-CREATE TABLE IF NOT EXISTS `datos_personales` (
+CREATE TABLE IF NOT EXISTS `forms` (
   `id` int NOT NULL,
-  `nombres` varchar(50),
-  `apellidos` varchar(50),
-  `direccion` varchar(50),
+  `nombre` varchar(50),
   `telefono` varchar(10),
   `email` varchar(70),
-  `documento` varchar(70),
-  `fecha_nacimiento` varchar(11),
-  `curso` varchar(5),
-  `horario` varchar(50),
-  `Lunes` bit,
-  `Martes` bit,
-  `Miercoles` bit,
-  `Jueves` bit,
-  `Viernes` bit,
-  `Pago_Contado_Moneda` bit,
-  `Pago_Tarjeta` bit,
-  `num_contrato` varchar(20),
-  `persona_que_atendio` varchar(50),
-  `fecha_inscripcion` date
+  `mensaje` varchar(300)
+
 );
 
-ALTER TABLE `datos_personales`
+ALTER TABLE `forms`
  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `datos_personales`
+ALTER TABLE `forms`
 MODIFY `id` int NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
 
-/*show tables;
-select * from datos_personales;*/
